ca950e2ae0cb0483b713b720494009f0cb93ed71
Nicolas <nialfonso12nick@gmail.com>
2023-03-30 23:44:04 -0500

    logica de añadir compra y disminuir

--- carrito.js
+++ carrito.js
@@ -102,33 +102,100 @@ fetch('https://fakestoreapi.com/products')
                     
   jplist.init();
  
-    $(".item .content-up .bton-det").on("click", function () {
-        $('.offcanvas .offcanvas-body .content-modal').remove()
-        getInfo($(this).closest(".item").attr('id'), $(".item"))
-        console.log($(this).closest(".item").attr('id'));
+    $(".store").on("click", ".item .content-up .bton-det", function () {
+        $('.offcanvas .offcanvas-body .content-modal').remove() //eliminar la informacion anterior al darle click
+        getInfo($(this).closest(".item").attr('id'), $(".item")) //tomar el id del contenedor con clase item
+        //console.log($(this).closest(".item").attr('id'));
     });
 
   
   const getInfo = (id, value) => {
-   const container = $("#" + id); // seleccionar el contenedor con el ID dado
-  const desc = container.find(".desc").text(); // obtener el texto dentro del contenedor con clase "desc"
-  const title = container.find(".title").text(); // obtener el texto dentro del contenedor con clase "title"
-  const price = container.find(".price").text(); // obtener el texto dentro del contenedor con clase "price"
-  const img = container.find("img").attr("src")
-  let canva = `<div class="content-modal row">
-                  <div class="col-6">
-                  <img src="${img}">
-                  </div>
-                  <div class="col-6">
-                    <p class="desc">${desc}></p>
-                    <p class="price">${price}</p>
-
-                  </div>
-
-                </div>`
-  //console.log(desc, title, price); // hacer algo con la información obtenida
-  $(".offcanvas .offcanvas-title").text(title);
-  $(".offcanvas .offcanvas-body").append(canva);
+      //
+      const container = $("#" + id); // seleccionar el contenedor con el ID dado
+      const desc = container.find(".desc").text(); // obtener el texto dentro del contenedor con clase "desc"
+      const title = container.find(".title").text(); // obtener el texto dentro del contenedor con clase "title"
+      const price = container.find(".price").text(); // obtener el texto dentro del contenedor con clase "price"
+      const img = container.find("img").attr("src")
+      //console.log(desc, title, price); 
+      paintInfo(desc, title, price, img);
+    }
+    
+    
+    const paintInfo = (desc, title, price, img) => {
+      let canva = `<div class="content-modal row">
+                      <div class="col-6">
+                      <img src="${img}">
+                      </div>
+                      <div class="col-6">
+                        <p class="desc">${desc}></p>
+                        <p class="price">${price}</p>
+  
+                      </div>
+  
+                    </div>`
+                    $(".offcanvas .offcanvas-title").text(title); //obtener el titulo e insertarlo en el header del modal
+                    $(".offcanvas .offcanvas-body").append(canva); //insertar el elemento en el modal      
+      //PINTAR ELEMENTOS COMPRADOS
+      
+              
   }
-  })
+
+  $(".store").on("click", ".item .content-up .bton", function () {
+    const infoCompra= $(this).closest(".item").find(".title").text();
+    const idCompra= $(this).closest(".item").attr("id")
+    //console.log(infoCompra)
+    //console.log(idCompra)
+
+    $(this).each(function (index, element) {
+      // element == this
+      let contador = '';
+      if ($('[data-id="'+idCompra+'"]').length == 0) {
+        contador = 1;
+        let compra = `<div class="col-12 item-compra animate__animated animate__fadeInLeft" data-id="${idCompra}">
+                        <p>${infoCompra}</p>
+                        <span class="cantidad">${contador}</span>
+                        <button class="dis-cant">-</button>
+                        <button class="aum-cant">+</button>
+                        <button class="delete-shop">X</button>
+                      </div>`
+    
+        $(".compras .row").append(compra);    
+      } else {
+        let cantidadElement = $('[data-id="'+idCompra+'"]').find('.cantidad');
+        contador = parseInt(cantidadElement.text()) + 1;
+        cantidadElement.text(contador);
+        //console.log(contador);
+      }
+    });
+
+
+
+    $('.delete-shop').on('click', function() {
+      $(this).closest('.item-compra').remove();
+    });
+
+    
+
+
+   
+});
+
+})
   .catch(error => console.error(error));
+
+  $(document).on("click", ".dis-cant", function(){
+    let cantidadElement = $(this).prev(".cantidad");
+    let cantidad = parseInt(cantidadElement.text());
+      cantidad--;
+      cantidadElement.text(cantidad);
+     if(cantidad == 0){
+      $(this).closest(".item-compra").remove()
+    }
+  });
+  $(document).on("click", ".aum-cant", function(){
+    let cantidadElement = $(this).prev().prev(".cantidad");
+    let cantidad = parseInt(cantidadElement.text());
+      cantidad++;
+      cantidadElement.text(cantidad);
+   
+  });
